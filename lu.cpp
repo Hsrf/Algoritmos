936474d2437d849f401e96ab86c8821354a454a1
Hsrf <38447080+Hsrf@users.noreply.github.com>
2018-08-31 14:27:55 -0300

    Receveing and categorizing inputs

--- lu.cpp
+++ lu.cpp
@@ -23,9 +23,10 @@ public:
 
     void enqueue(int dataM, int dataT){
         Node *aux = new Node(dataM, dataT);
-        rear->next = aux;
+        rear -> next = aux;
         rear = aux;
     }
+
     Node* dequeue(){
         Node *aux = front -> next;
         front -> next = front-> next -> next;
@@ -57,19 +58,33 @@ public:
 
 
 int main(){
+    //Receiving processes and enqueueing them
     int amountCompanies, amountProcesses, magistrate, time;
-
-    //Receiving inputs
     scanf("%d",&amountCompanies);
     Queue *arrayQueue[amountCompanies];
     for(int i = 0; i < amountCompanies; i++){
         scanf("%d",&amountProcesses);
+        arrayQueue[i] = new Queue();
         for(int j = 0; j < amountProcesses; j++){
             scanf("%d",&magistrate);
             scanf("%d",&time);
             arrayQueue[i] -> enqueue(magistrate, time);
         }
     }
+
+    //Receiving magistrades & creating/initializing their stacks
+    int amountMagistrades;
+    scanf("%d", &amountMagistrades);
+    Stack *arrayStack[amountMagistrades];
+    int magistradeHours[amountMagistrades];
+    for(int k = 0; k < amountMagistrades; k++){
+        arrayStack[k] = new Stack();
+        scanf("%d", &magistradeHours[k]);
+
+    }
+
+    //Distributing processes between magistrades
+
 
 
 }
