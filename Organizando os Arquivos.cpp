68cfde62cce3795574c936864dff966932976e1a
Hsrf <38447080+Hsrf@users.noreply.github.com>
2018-09-14 19:27:30 -0300

    Update Organizando os Arquivos.cpp
    O CLion tá me deixando puto.

--- Organizando os Arquivos.cpp
+++ Organizando os Arquivos.cpp
@@ -1,23 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//ainda falta implementar node e fila
 
-int chave(string nome){
-    int soma = 0;
-    for(int i = 0; i < nome.length(); i++) {
-        soma = soma + ((i + 1) * nome.at(i));
+//Fila
+struct Node{
+    int id;
+    string nome;
+    Node *next;
+    Node(string nome, int id){
+        this->nome = nome;
+        this->id = id;
+        this->next = NULL;
     }
-    return soma;
+}typedef Node;
+
+
+class Queue{
+private:
+    Node *front, *rear;
+public:
+    Queue(){
+        front = new Node("",0);
+        rear = front;
+    }
+
+    void enqueue(string nome, int id){
+        Node *aux = new Node(nome, id);
+        rear -> next = aux;
+        rear = aux;
+    }
+
+    Node* dequeue(){
+        Node *aux = front -> next;
+        front -> next = front-> next -> next;
+        return aux;
+    }
+};
+
+
+
+//Hash Tabling
+int chave(string nome){
+    int k = 0;
+    for(int i = 0; i < nome.length(); i++) {
+        k = k + ((i + 1) * nome.at(i));
+    }
+    return k;
 }
 
 int dispersao(int k, int m){
     return k % m;
 }
 
-int buscaBinaria(int valor, int vetor[]){
+
+
+int buscaBinaria(int valor, int vetor[], int tamanhoVetor){
     //Array precisa estar ordenado para binary search funcionar
-    int l = 0, r = (sizeof(vetor)/sizeof(vetor[0]) - 1);
+    int l = 0, r = tamanhoVetor;
     int m;
     while(l <= r){
         m = ((l+r) / 2);
@@ -34,37 +73,41 @@ int buscaBinaria(int valor, int vetor[]){
 
 int main() {
     int qntdArquivosAntigos, qntdGavetasNovas, qntdArquivosTransferidos, qntdArquivosConsultados;
-    scanf("%d", qntdArquivosAntigos);
+    scanf("%d", &qntdArquivosAntigos);
     string nomesArquivos[qntdArquivosAntigos];
     int idArquivos[qntdArquivosAntigos];
 
     //Recebendo todos os arquivos antigos
     for(int i = 0; i < qntdArquivosAntigos; i++){
-        scanf("%s", nomesArquivos);
-        scanf("%d", idArquivos);
+        scanf("%s", &nomesArquivos);
+        scanf("%d", &idArquivos);
     }
 
-    scanf("%d", qntdGavetasNovas);
-    scanf("%d", qntdArquivosTransferidos);
+    scanf("%d", &qntdGavetasNovas);
+    Queue gavetas[qntdGavetasNovas];
+    scanf("%d", &qntdArquivosTransferidos);
     int idArquivosTransferidos[qntdArquivosTransferidos];
 
     //Rebendo id de arquivos que vao ser transferidos
     for(int i = 0; i < qntdArquivosTransferidos; i++){
-        scanf("%d", idArquivosTransferidos[i]);
+        scanf("%d", &idArquivosTransferidos[i]);
     }
 
 
-    scanf("%d", qntdArquivosConsultados);
+    scanf("%d", &qntdArquivosConsultados);
     string nomesArquivosConsultados[qntdArquivosConsultados];
 
     //Recebendo nomes de arquivos para serem consultados;
     for(int i = 0; i < qntdArquivosConsultados; i++){
-        scanf("%s",nomesArquivosConsultados[i]);
+        scanf("%s", &nomesArquivosConsultados[i]);
     }
 
-    //Fim das entradas;
-
-    //fazer as tretas aqui
+    int resultadoBusca;
+    //Fazendo o hashing para arquivos que serao transferidos
+    for(int i = 0; i < qntdArquivosTransferidos; i++){
+        resultadoBusca = buscaBinaria(idArquivosTransferidos[i], idArquivos, qntdArquivosAntigos - 1);
+        gavetas[dispercao(chave(nomeArquivos[resultadoBusca]), qntdGavetasNovas)].enqueue(nomesArquivos[resultadoBusca], idArquivosTransferidos[i]);
+    }
 
     return 0;
 }
